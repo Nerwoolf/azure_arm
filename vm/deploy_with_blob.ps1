06c795e5143350e355ae68a1db18e07b0ef11414
nerwoolf <nerwoolf123@gmail.com>
2018-10-10 17:31:16 +0300

    Big update with sas token

---
+++ vm/deploy_with_blob.ps1
@@ -0,0 +1,119 @@
+<#
+ .SYNOPSIS
+    Deploys a template to Azure
+
+ .DESCRIPTION
+
+ .PARAMETER subscriptionId
+    The subscription id where the template will be deployed.
+
+ .PARAMETER resourceGroupName
+    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
+
+ .PARAMETER resourceGroupLocation
+    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
+
+ .PARAMETER deploymentName
+    The deployment name.
+
+ .PARAMETER templateFilePath
+    Optional, path to the template file. Defaults to template.json.
+
+ .PARAMETER parametersFilePath
+    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+#>
+
+param(
+    [Parameter(Mandatory = $false)]
+    [string]$subscriptionId = "69f423df-297f-4625-b184-1cd5027cdba8",
+
+    [Parameter(Mandatory = $True)]
+    [string]$resourceGroupName="minsk",
+ 
+    [Parameter(Mandatory = $true)]
+    [string]$resourceGroupLocation="westeurope",
+
+    [Parameter(Mandatory = $false)]
+    [string]$deploymentName = "Task 4 - Deploy ARM's",
+
+    [string]$templateUri = "https://raw.githubusercontent.com/Nerwoolf/azure_arm/master/vm/azuredeploy.json",
+
+    [string]$parametersFilePath = "s.json"
+)
+begin{
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    # Variables define
+    $ARMGithubPrefix = "https://raw.githubusercontent.com/Nerwoolf/azure_arm/master/vm/"
+    $DSCGithubPrefix ="https://github.com/Nerwoolf/azure_arm/raw/master/vm/iissettingup.ps1.zip"
+    $FilesToDownload = [ordered]@{
+
+                                    "azuredeploy" = "azuredeploy.json"
+                                    "keyvault" = "keyvault.json"
+                                    "virtnet" = "virtnet-with-pip.json"
+                                    "vmdeploy" = "vmdeploy.json"
+                                    "vmextension" = "vmextension.json"
+                                    "storageaccout" = "storageaccount.json"
+                                    "nsg" = "nsg.json"
+                                    "deploy_with_blob" = "deploy_with_blob.ps1"
+                                    
+                                }
+    $storAccName = $("storacc"+$resourceGroupName+$resourceGroupLocation).ToLower()
+    $storAccContainerName = ("automation{0}{1}" -f $resourceGroupName, $resourceGroupLocation)
+    $tempDownloadFolder = "$env:USERPROFILE\desktop\temp"
+    $resourceGroupName = $resourceGroupName.ToLower()
+}
+process {
+    
+
+    # sign in
+    Write-host -ForegroundColor Green "Loging to azure subscription"
+    $SubCheck = Get-AzureRmSubscription -SubscriptionId $subscriptionId -ErrorAction SilentlyContinue
+    if (!$SubCheck) {
+        Connect-AzureRmAccount 
+        Select-AzureRmContext -SubscriptionID $subscriptionId
+    }
+
+    #Create or check for existing resource group
+    $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+    if (!$resourceGroup) {
+        Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+        New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+    }
+    else {
+        Write-Host "Using existing resource group $resourceGroupName";
+    }
+
+    # Create storage for artifatcs 
+    New-AzureRmStorageAccount -ResourceGroupName $resourceGroupName -Location $resourceGroupLocation -Name $storAccName -SkuName Standard_LRS 
+    Set-AzureRmCurrentStorageAccount -ResourceGroupName $resourceGroupName -Name $storAccName
+    
+    New-AzureStorageContainer -Name $storAccContainerName -Permission Off
+    
+    # Download files to container
+    
+    foreach ($item in $FilesToDownload.GetEnumerator()){
+            $link = ("{0}{1}" -f $ARMGithubPrefix ,$item.value )
+            New-Item -ItemType Directory -Path $tempDownloadFolder -ErrorAction SilentlyContinue 
+
+            Invoke-WebRequest -Uri $link -OutFile "$tempDownloadFolder\$($item.value)"
+    }
+    Invoke-WebRequest -Uri $DSCGithubPrefix -OutFile "$tempDownloadFolder\iissettingup.ps1.zip"
+    $files =  Get-ChildItem  -Path $tempDownloadFolder
+    $files.FullName | ForEach-Object {Set-AzureStorageBlobContent -Container $storAccContainerName -File $_}
+    
+    # Create SAS token to storage account
+    
+    $sasToken = New-AzureStorageAccountSASToken -Service Blob -ResourceType Service, Container , Object -Permission "r" -Protocol HttpsOnly -ExpiryTime $(get-date).AddDays(2)
+    
+      
+
+    # Start the deployment
+    Write-Host "Starting deployment...";
+    if (Test-Path $parametersFilePath) {
+        New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri ($templateUri+$sasToken) -TemplateParameterFile $parametersFilePath -containerSASToken $sasToken
+    }
+    else {
+        New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri ($templateUri+$sasToken) -containerSASToken $sasToken
+    }
+    Remove-Item -Path $tempDownloadFolder -Force
+}

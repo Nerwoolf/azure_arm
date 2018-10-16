1df6041b6b466712b1871a8b461ad6ee14940382
nerwoolf <nerwoolf123@gmail.com>
2018-10-16 22:08:38 +0300

    need sas

--- vm/deploy.ps1
+++ vm/deploy.ps1
@@ -91,4 +91,50 @@ process{
         New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateUri $templateUri -secretsObject $password -armLink $armLink
     }
 
+    # Wait minute for backup agent
+    Write-host -ForegroundColor Yellow "Waiting for backup agent provision be complete"
+
+    # Trigger backup 
+    $backupVault = Get-AzureRmRecoveryServicesVault -ResourceGroupName $resourceGroupName
+    $backupVault | Set-AzureRmRecoveryServicesVaultContext
+    
+    # Starting backup job
+    $namedContainer = Get-AzureRmRecoveryServicesBackupContainer -ContainerType "AzureVM" 
+    $item = Get-AzureRmRecoveryServicesBackupItem -Container $namedContainer -WorkloadType "AzureVM"
+    $job = Backup-AzureRmRecoveryServicesBackupItem -Item $item
+
+    # Wait for backup job complete
+    Write-host -ForegroundColor Yellow "waiting for backup" 
+    do {
+       $progress = Get-AzureRmRecoveryservicesBackupJob –Status "InProgress"
+       Write-host -NoNewline "." 
+       Start-Sleep -Seconds 10
+    } while ($progress)
+    
+    # Get recovery points
+    
+    $backupitem = Get-AzureRmRecoveryServicesBackupItem -Container $namedContainer  -WorkloadType "AzureVM"
+    $recoveryPoint = Get-AzureRmRecoveryServicesBackupRecoveryPoint -Item $backupitem
+    
+    # Create RG to restore
+    $recoveryRgName = "recovery-group-module7"
+    $recoveryRg = New-AzureRmResourceGroup -Name $recoveryRgName -Location $resourceGroupLocation
+    
+    # Create Storage account in recovery RG
+    $storaccount = New-AzureRmStorageAccount -Name "module7recoverystor" -ResourceGroupName $recoveryRgName -Location $resourceGroupLocation -SkuName Standard_LRS -Kind StorageV2
+    
+    # Start recovery from last recovery point
+    Write-host -ForegroundColor Yellow "Starting recovery to another RG and storage account"
+    $restorejob = Restore-AzureRmRecoveryServicesBackupItem -RecoveryPoint $recoveryPoint[0] `
+                                                            -StorageAccountName $storaccount.StorageAccountName `
+                                                            -StorageAccountResourceGroupName "$recoveryRgName"
+    Start-Sleep 2
+    Write-Host -ForegroundColor Yellow "Now waiting for recovery job complete"
+    Wait-AzureRmRecoveryServicesBackupJob -Job $restorejob -Timeout 43200
+    
+    # Get blob url from storage account
+    $recoveryAccContainer = Get-AzureStorageContainer -Context $storaccount.Context
+    $recoveryAccBlob = Get-AzureStorageBlob -Context $storaccount.Context -Container $recoveryAccContainer.Name | Where-Object -Property Length -ge 1GB
+
+
 }
